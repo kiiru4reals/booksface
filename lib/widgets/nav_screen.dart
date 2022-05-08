038d9d3388f38cd6ad573b350366b2a7acce8d6b
kiiru4reals <smaina991@gmail.com>
2022-05-08 13:42:48 +0300

    AppBar moves to top when size is less than 800

--- lib/widgets/nav_screen.dart
+++ lib/widgets/nav_screen.dart
@@ -1,5 +1,8 @@
+import 'package:booksface/data/data.dart';
 import 'package:booksface/screens/home_page.dart';
+import 'package:booksface/widgets/custom_app_bar.dart';
 import 'package:booksface/widgets/custom_tab_bar.dart';
+import 'package:booksface/widgets/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 
@@ -30,16 +33,28 @@ class _NavigationScreenState extends State<NavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return DefaultTabController(
       length: _icons.length,
       child: Scaffold(
-        body: IndexedStack(index: _selectedIndex,
-        children: _screens,),
-        bottomNavigationBar: CustomTabBar(
+        appBar: Responsive.isDesktop(context) ? PreferredSize(preferredSize: Size(
+          screenSize.width, 100,
+
+        ),
+        child: CustomAppBar(
+          currentUser: currentUser,
           icons: _icons,
           selectedIndex: _selectedIndex,
           onTap: (index) => setState(() => _selectedIndex = index),
         ),
+        ) : null,
+        body: IndexedStack(index: _selectedIndex,
+        children: _screens,),
+        bottomNavigationBar: !Responsive.isDesktop(context)  ? CustomTabBar(
+          icons: _icons,
+          selectedIndex: _selectedIndex,
+          onTap: (index) => setState(() => _selectedIndex = index),
+        ) : SizedBox.shrink(),
 
       ),
     );
